e9b6fbab473e2147ee3d58b0306abda0657928ad
Leyla Becker <git@jan-leila.com>
2022-06-13 21:53:22 -0500

    fixed grouping of tokens in lexer

--- src/lexer.js
+++ src/lexer.js
@@ -1,5 +1,46 @@
 const moo = require('moo');
 
+let keywords = [
+	'import', 'from', 'as', 'export', 'default',
+	'if', 'else', 'switch', 'case', 'default', 'do', 'while', 'for', 'break', 'continue', 'return',
+	'try', 'with', 'handle', 'use', 'throw', 'catch',
+	'enum', 'struct', 'function', 'event', 'in', 'out', 'monad', 'bind', 'reduce', 'extends',
+	'any', 'symbol', 'boolean', 'int', 'float', 'string', 'char', 'func',
+	'null',
+];
+
+let assignment = [
+	'=??', '=&&', '=||',
+	'=<<<', '=>>>', '=<<', '=>>',
+	'=&', '=|', '=^',
+	'=**', '=%', '=*', '=/', '=+', '=-',
+	'=',
+];
+
+let misc_tokens = [
+	'...',
+	'<<<', '>>>',
+
+	'<<=', '>>=',
+
+	'??',
+	'==',
+	'&&', '||', '<<', '>>',
+	'**', '++', '--',
+
+	'=>',
+	'/>', '</',
+	'!=', '>=', '<=',
+	'?.', '?(', '?[',
+
+	'?', ':',
+	'.', '(', '[',
+	',', '<', '>', ')', ']',
+	'!', '~', '&', '|', '^',
+	'+', '-', '*', '/', '%',
+];
+
+
 let lexer = moo.states({
 	main: {
 		whitespace: [{ match: /[ \n\t]+/, lineBreaks: true }, /[ \t]+/s],
@@ -18,53 +59,64 @@ let lexer = moo.states({
 			/#[0-9a-fA-F]{8}/,
 		],
 
-		regex: /\/(?=(?:(?:\\.)?[^\\\/\n]+)+\/)/,
-
 		string: [
 			{ match: /".*?(?<!\\)(?:\\\\)*"/s, lineBreaks: true },
 			{ match: /'.*?(?<!\\)(?:\\\\)*'/s, lineBreaks: true }
 		],
-		lit_str_start: { match: '`', push: 'str_lit' },
+		template_string_start: { match: '`', push: 'template_string' },
+
+		regex: /\/(?=(?:(?:\\.)?[^\\\/\n]+)+\/)/,
 
 		lcbracket: { match: '{', push: 'main' },
 		rcbracket: { match: '}', pop: true },
 
-		misc_tokens: [
-			'<<<=', '>>>=',
-			'...', '<<<', '>>>',
-
-			'<<=', '>>=', '&&=', '||=', '<<=', '>>=', '**=',
-			'??', '++', '--', '==', '<<', '>>', '&&', '||', '<<', '>>', '**',
-
-			'+=', '-=', '*=', '/=', '%=', '&=', '|=', '~=', '^=', '=>', '/>', '</', '!=', '>=', '<=',
-			'=', '<', '>', '?', ':', '.', ',', '(', ')', '[', ']', '&', '|', '^', '+', '-', '*', '/', '%', '!', '~',
-		],
+		misc_tokens: {
+			match: misc_tokens,
+			type: moo.keywords({
+				assignment,
+			}),
+		},
+		assignment,
 
 		identifier: {
 			match: /\w+/, type: moo.keywords({
-				keywords: [
-					'import', 'from', 'as', 'export', 'default',
-					'if', 'else', 'switch', 'case', 'default', 'do', 'while', 'for', 'break', 'continue', 'return',
-					'try', 'with', 'handle', 'use', 'throw', 'catch',
-					'enum', 'struct', 'function', 'effect', 'from', 'to', 'monad', 'bind', 'reduce',
-					'let', 'symbol', 'boolean', 'int', 'float', 'string', 'char', 'func',
-					'null',
-				],
+				keywords,
 			})
 		},
+		keywords,
 		break: ';',
 	},
-	str_lit: {
-		interp: { match: '${', push: 'main' },
-		str_content: { match: /(?:(?:\\.)+|[^\\$`]+)+/, lineBreaks: true },
-		lit_str_end: { match: '`', pop: true },
+	template_string: {
+		template_string_interpreter: { match: '${', push: 'main' },
+		template_string_content: { match: /(?:(?:\\.)+|[^\\$`]+)+/, lineBreaks: true },
+		template_string_end: { match: '`', pop: true },
 	},
 	regex: {
 		// TODO: replace with real regex parsing
-		regex: /(?:(?:\\.)?[^\\\/\n]+)+/,
-		// regex_literals: /(?:\\.)+/,
-		regex_end: { match: '/', pop: true },
+		regex_content: /(?:(?:\\.)?[^\\\/\n]+)+/,
+		// match the end and any flags that go with it,
+		regex_end: { match: /\/[gmiy]*/, pop: true },
 	}
 });
 
-module.exports = lexer;
+module.exports = {
+	next(){
+		let token;
+		do {
+			token = lexer.next()
+		} while (token?.type === 'comment');
+		return token
+	},
+	save(){
+		return lexer.save();
+	},
+	reset(chunk, info){
+		return lexer.reset(chunk, info);
+	},
+	formatError(token){
+		return lexer.formatError(token);
+	},
+	has(name){
+		return lexer.has(name);
+	}
+};
