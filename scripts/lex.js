ce6478ecac7fcd7577d0c2d9dc0d5daa0e33cdb2
Leyla Becker <git@jan-leila.com>
2023-05-24 01:45:10 -0500

    changed test file name in lex script

--- scripts/lex.js
+++ scripts/lex.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const lexer = require('../src/engine/lexer.js');
 
-let file = fs.readFileSync('examples/index.qk', "utf-8");
+let file = fs.readFileSync('examples/example-project/index.qk', "utf-8");
 lexer.reset(file);
 
 let line;
